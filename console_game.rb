ebce54865a67887f8370a3688cd23d381b2aeab9
Ayesha-42 <62644991+Ayesha-42@users.noreply.github.com>
2022-06-29 15:22:14 +1000

    first commit

---
+++ console_game.rb
@@ -0,0 +1,38 @@
+def main()
+    puts "Game: HiLo"
+
+    #computer chooses a number from 1 to 100 (inclusive)
+    comp_choice = rand(101)
+    
+    number_of_choices = 1
+    
+   # do-while loop for each of the 6 chances 
+   loop do
+        #gets users guess
+        user_choice  = gets.chomp
+        
+        #checks if not nil
+        user_choice!="" ? user_choice = user_choice.to_i : (puts "Please enter a valid integer value" ; next)
+
+        if user_choice ==comp_choice 
+            puts "you guessed the number correctly"
+            break
+        
+        elsif user_choice < comp_choice
+            puts "you guessed less that the required number"
+        
+        else
+            puts "you guessed more than the required number"
+        end
+
+        if number_of_choices>5
+            puts("You've run out of chances to guess. Better luck next time")
+            break
+        end
+        
+        number_of_choices+=1
+    end
+
+end
+
+main()
